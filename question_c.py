1c349ee45bb7fbde5ca635f459e54100fdd214f9
marclacerna <marcyvan.lacerna@gmail.com>
2018-10-04 13:25:50 -0400

    adding json interface

--- question_c.py
+++ question_c.py
@@ -1,3 +1,46 @@
+import json
+
+
+class GetFile:
+    """
+    Read/ Load json file, The jsons will contain geolocations
+    """
+
+    def __init__(self, path):
+        self.path = path
+        self.json_data = None
+        self.ip_addr = None
+        self.country = None
+        self.city = None
+        self.longlat = None
+
+    def set_ip_addr(self, ip_addr):
+        self.ip_addr = ip_addr
+
+    def set_country(self, country):
+        self.country = country
+
+    def set_city(self, city):
+        self.city = city
+
+    def set_long_lat(self, longlat):
+        self.longlat = longlat
+
+    def read_json(self):
+        with open(self.path, 'r') as read_file:
+            self.json_data = json.load(read_file)
+
+    def add_to_json(self, attrib):
+        if attrib not in self.json_data:
+            self.json_data[attrib] = list()
+        else:
+            self.json_data[attrib].append({'ip address': self.ip_addr,
+                                           'country': self.country,
+                                           'city': self.city,
+                                           'long/lat': self.longlat})
+
+            with open(self.path, 'w') as load_data:
+                json.dump(load_data)
 
 
 class LRUCache:
